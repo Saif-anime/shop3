5c8e2f297f45c6d43fba298eb00dcb5522d3eefb
Saif-anime <saif817883@gmail.com>
2024-04-07 19:41:35 +0530

    kajogari

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     env: {
-        // API_FETCH_URL: 'https://backend-kajori-1.onrender.com',
-        API_FETCH_URL: 'http://localhost:3001',
+        API_FETCH_URL: 'https://backend-kajori-2.onrender.com',
+        // API_FETCH_URL: 'http://localhost:3001',
     },
     images: {
-        // domains: ['backend-kajori-1.onrender.com'],
-        domains: ['localhost'],
+        domains: ['https://backend-kajori-2.onrender.com'],
+        // domains: ['localhost'],
       },
   }
